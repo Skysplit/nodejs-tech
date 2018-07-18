Skysplit/musicary

Add routing between two sample nextjs pages

src/client/pages/home.tsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Link from '@next/link';
 
 export interface IndexPageProps {
 
@@ -7,7 +8,13 @@ export interface IndexPageProps {
 export default class IndexPage extends Component<IndexPageProps> {
   render() {
     return (
-      <h1>Hello world</h1>
+      <Fragment>
+        <h1>Hello world</h1>
+
+        <Link route="test">
+          <a>Go to some page</a>
+        </Link>
+      </Fragment>
     );
   }
 }

Skysplit/nodejs-tech

src/client/pages/home.tsx
@@ -1,9 +1,16 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
+import Link from '@next/link';
 
 export default class HomePage extends PureComponent<{}> {
   render() {
     return (
-      <h1>Hello world</h1>
+      <Fragment>
+        <h1>Hello world</h1>
+
+        <Link route="test">
+          <a>Go to some page</a>
+        </Link>
+      </Fragment>
     );
   }
 }